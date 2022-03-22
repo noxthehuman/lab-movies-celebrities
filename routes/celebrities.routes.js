91901a1ea0087560e92e7f938e0f3973791bea7d
Nox <benaziz.ines@gmail.com>
2022-03-22 13:16:33 +0100

    iteration 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,11 +3,11 @@ const Celebrity = require("../models/Celebrity.model");
 const router = require("express").Router();
 
 // all your routes here
-router.get('/celebrities/create', (req, res) => {
+router.get('/celebrities/create', (req, res, next) => {
     res.render('celebrities/new-celebrity')
 }) 
 
-router.post('/celebrities/create', async (req, res) => {
+router.post('/celebrities/create', async (req, res, next) => {
     try {
         const celebToCreate = req.body
         await Celebrity.create(celebToCreate)
@@ -16,6 +16,16 @@ router.post('/celebrities/create', async (req, res) => {
     }
     catch {
         res.redirect('/celebrities/new-celebritiy')
+    }
+})
+
+router.get('/celebrities', async (req, res, next) => {
+    try {
+        const celebrities = await Celebrity.find()
+        res.render('celebrities/celebrities', {celebrities} )
+    }
+    catch(error) {
+        console.error(error)
     }
 })
 
