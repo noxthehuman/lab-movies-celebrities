59acb13aa71a599a8e2797690c8c99e7f91c3603
Nox <benaziz.ines@gmail.com>
2022-03-22 15:02:05 +0100

    iteration 9 done

---
+++ models/Movie.model.js
@@ -0,0 +1,14 @@
+require('../db')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieModel = new Schema({
+    title: String,
+    genre: String,
+    plot: String,
+    cast: { type: [Schema.Types.ObjectId], ref: 'Celebrity' }
+})
+
+const Movie = mongoose.model('Movie', movieModel)
+
+module.exports = Movie
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,59 @@
-const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
+const Movie = require('../models/Movie.model')
+const router = require("express").Router()
 
 // all your routes here
+router.get('/movies/create', async (req, res, next) => {
+    const celebrities = await Celebrity.find()
+    res.render('movies/new-movie', {celebrities})
+})
+
+router.post('/movies/create', async (req, res, next) => {
+    try {
+        const movieToCreate = req.body
+        await Movie.create(movieToCreate)
+
+        res.redirect('/movies')
+    }
+    catch {
+        res.redirect('movies/new-movie')
+    }
+})
+
+router.get('/movies', async (req, res, next) => {
+    try {
+        const movies = await Movie.find()
+        res.render('movies/movies', {movies})
+    }
+    catch(error) {
+        console.error(error)
+    }
+})
+
+router.get('/movies/:id', async (req, res, next) => {
+    try {
+
+    const id = req.params.id
+    const movie = await Movie.findById(id).populate("cast")
+
+    res.render('movies/movie-details', {movie})
+    }
+    catch(error) {
+        console.error(error)
+    }
+})
+
+router.post('/movies/:id/delete', async (req, res, next) => {
+    try {
+
+    const id = req.params.id
+    await Movie.findByIdAndDelete(id)
+
+    res.redirect('/movies')
+    }
+    catch(error) {
+        console.error(error)
+    }
+})
 
 module.exports = router;
